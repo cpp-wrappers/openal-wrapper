58a2b85fe0b489ba2f9f1d1ede9ac7ed2c1089b6
fewizz <hazeevaidar@gmail.com>
2019-05-19 22:36:27 +0300

    relocating, refactor build script

--- build.sh
+++ build.sh
@@ -1,29 +1,12 @@
-if [[ -z $CXX ]]; then
-    echo you must specify CXX variable;
-    exit
-fi
+mkdir -p build
+cd build
+rm *
 
-cxxargs=\
-"-c \
---std=c++17 \
--I../../include"
+$CXX $CXXFLAGS -c -std=c++17 \
+-I../src \
+../src/al/internal.cpp \
+../src/al/buffer.cpp \
+../src/al/source.cpp \
+../src/alc/device.cpp
 
-cxxargs=$cxxargs $CXXARGS
-
-mkdir -p build/al
-cd build/al
-$CXX \
-$cxxargs \
-../../src/al/internal.cpp \
-../../src/al/buffer.cpp \
-../../src/al/source.cpp
-
-cd ../..
-mkdir -p build/alc
-cd build/alc
-$CXX \
-$cxxargs \
-../../src/alc/device.cpp
-
-cd ..
-ar -rs libopenal-wrapper.a al/internal.o al/buffer.o al/source.o alc/device.o
+ar -rs libopenal-wrapper.a internal.o buffer.o source.o device.o
